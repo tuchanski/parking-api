dffbde5ca63fb0d0965738b6066ac10c2fd28cb3
Tuchanski <gui.rocha2004@gmail.com>
2025-04-23 19:04:13 -0300

    Create POST user method

--- src/main/java/com/tuchanski/parking_api/services/UserService.java
+++ src/main/java/com/tuchanski/parking_api/services/UserService.java
@@ -1,8 +1,10 @@
 package com.tuchanski.parking_api.services;
 
+import com.tuchanski.parking_api.entities.User;
 import com.tuchanski.parking_api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 
@@ -11,4 +13,8 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
--- src/main/java/com/tuchanski/parking_api/web/controllers/UserController.java
+++ src/main/java/com/tuchanski/parking_api/web/controllers/UserController.java
@@ -1,7 +1,12 @@
 package com.tuchanski.parking_api.web.controllers;
 
+import com.tuchanski.parking_api.entities.User;
 import com.tuchanski.parking_api.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        User userToBeCreated = userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userToBeCreated);
+    }
 
 }
