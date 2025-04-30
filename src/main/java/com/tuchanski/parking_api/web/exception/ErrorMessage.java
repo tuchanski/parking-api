1bd507b02b4d2228cdc78edafca7d6d583719311
Tuchanski <gui.rocha2004@gmail.com>
2025-04-30 20:25:50 -0300

    Add ErrorMessage class to treat exceptions

---
+++ src/main/java/com/tuchanski/parking_api/web/exception/ErrorMessage.java
@@ -0,0 +1,50 @@
+package com.tuchanski.parking_api.web.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter @ToString
+public class ErrorMessage {
+
+    private String path;
+    private String httpMethod;
+    private int status;
+    private String statusText;
+    private String message;
+
+    private Map<String, String> errors;
+
+    public ErrorMessage() {
+    }
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message) {
+        this.path = request.getRequestURI();
+        this.httpMethod = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+    }
+
+    public ErrorMessage(HttpServletRequest request, HttpStatus status, String message, BindingResult bindingResult) {
+        this.path = request.getRequestURI();
+        this.httpMethod = request.getMethod();
+        this.status = status.value();
+        this.statusText = status.getReasonPhrase();
+        this.message = message;
+        addErrors(bindingResult);
+    }
+
+    public void addErrors(BindingResult bindingResult) {
+        this.errors = new HashMap<>();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            this.errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+    }
+}
